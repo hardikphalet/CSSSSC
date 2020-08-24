32febcae7c0db81926a07cdcca07af56af23e2e7
Harsh Bindal <harshbin06@gmail.com>
2020-08-24 17:01:54 +0530

    Change it to the flask template front end

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+   
+    return render_template('about.html')
+
+
+
+@app.route("/contact")
+def contact():
+    
+    return render_template('contact.html')
+
+@app.route("/Post")
+def Post():
+    
+    return render_template('Post.html')
+
+
+app.run(debug=True)
+
+
+  
