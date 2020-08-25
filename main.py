029a3c7d5e5cd8544a3e9697d426b4210c50ec19
Harsh Bindal <harshbin06@gmail.com>
2020-08-25 16:41:10 +0530

    Update Database and Make contact form working

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
 
-app=Flask(__name__)
 
+
+
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/compscsoc' 
+# THIS IS MYSQL database connection  and the syntax is 'mysql://USER:PASSWORD@SERVER/DATABASE' I have no password so here my pass is blank 
+
+db = SQLAlchemy(app)  # INITIALIZE THE DATABASE
+
+class Contacts(db.Model):   # This Contact Class is for contact table 
+    QsnId = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(35), nullable=False)   
+    EmailId = db.Column(db.String(40), nullable=False)
+    PhoneNum = db.Column(db.String(12), nullable=False)
+    Msg = db.Column(db.String(300), nullable=False)
+    DT = db.Column(db.String(20), nullable=True)
+
+# Default value of nullabe is True so We make it False where for unique it is True so We dont mention we need column in any field
 
 
 @app.route("/")
@@ -17,17 +38,37 @@ def about():
 
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
+
 def contact():
-    
+    if(request.method=='POST'):
+        # Add Entry To the database
+        Name=request.form.get('Name')
+        EmailId=request.form.get('Email')
+        ContactNum=request.form.get('ContactNum')
+        Msg=request.form.get('MSG')
+
+
+        entry=Contacts(Name=Name, EmailId=EmailId, PhoneNum=ContactNum, Msg=Msg) # Here lhs are from class attribute and rhs are if ke andar wale
+        db.session.add(entry)
+        db.session.commit()
+
+
+
     return render_template('contact.html')
+
+
+
 
 @app.route("/Post")
 def Post():
-    
     return render_template('Post.html')
 
 
+
+
+app.run(debug=True)
+app.run(debug=True)
 app.run(debug=True)
 
 
